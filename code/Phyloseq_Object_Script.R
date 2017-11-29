6070fb42d2adc0b41dcde51868ed94ab7aa60dc1
Katelyn Kukar <kbkukar@usfca.edu>
2017-11-29 08:22:20 -0800

    Commit of Updates scripts for new data set

---
+++ code/Phyloseq_Object_Script.R
@@ -0,0 +1,160 @@
+# This is an R Script to make a phyloseq object to be used within an Amplicon Report
+# This script will utilize data collected from NCBI in an SRA table
+
+# Be sure to install these packages before running this script
+# They can be installed either with the intall.packages() function
+# or with the 'Packages' pane in RStudio
+
+# load general-use packages
+library("dplyr")
+library("tidyr")
+library("knitr")
+library("ggplot2")
+
+# this package allows for the easy inclusion of literature citations in our Rmd
+# more info here: https://github.com/crsh/citr
+# and here:
+# http://rmarkdown.rstudio.com/authoring_bibliographies_and_citations.html
+library("citr")
+
+# These are the primary packages well use to clean and analyze the data
+# this package needs to be installed from bioconductor -- it's not on CRAN
+# see info here: https://benjjneb.github.io/dada2/dada-installation.html
+library("dada2")
+
+# This to export a fasta of our final denoised sequence variants
+library("seqinr")
+
+# To install this you have to install from GitHub
+# See more info here: https://github.com/leffj/mctoolsr
+# run this -- install.packages("devtools")
+# and then this -- devtools::install_github("leffj/mctoolsr")
+library("mctoolsr")
+
+# And this to visualize our results
+# it also needs to be installed from bioconductor
+library("phyloseq")
+
+# Add colors
+library("RColorBrewer")
+
+
+
+# The primary graphs represent each of 20 samples original sequence error margins.
+# NOTE: Much of the following follows the DADA2 tutorials available here:
+# https://benjjneb.github.io/dada2/tutorial.html
+# Accessed October 19, 2017
+
+# set the base path for our input data files
+path <- "data/raw_data"
+
+# Sort ensures samples are in order
+filenames_forward_reads <- sort(list.files(path, pattern = ".fastq"))
+
+# Extract sample names, assuming filenames have format: SAMPLENAME.fastq
+sample_names <- sapply(strsplit(filenames_forward_reads, "\\."), `[`, 1)
+
+# Specify the full path to each of the filenames_forward_reads
+filenames_forward_reads <- file.path(path, filenames_forward_reads)
+
+
+# Plots the quality profiles of all twenty samples
+plotQualityProfile(filenames_forward_reads[1:10])
+
+#We can see from the quality profiles that most reads tend to get pretty bad in quality after around 200 bases. Therefore, we decided to set a maximum acceptable sequence length of 225 bases.
+
+# Place filtered files in filtered/ subdirectory
+# note this will fail if the directory doesn't exist
+filter_path <- file.path("output", "filtered")
+filtered_reads_path <- file.path(filter_path,
+                                 paste0(sample_names,
+                                        "_filt.fastq.gz"))
+
+# See ?filterAndTrim for details on the parameters
+# See here for adjustments for 454 data:
+# https://benjjneb.github.io/dada2/
+#     faq.html#can-i-use-dada2-with-my-454-or-ion-torrent-data
+filtered_output <- filterAndTrim(fwd = filenames_forward_reads,
+                                 filt = filtered_reads_path,
+                                 maxLen = 300,
+                                 maxN = 0, # discard any seqs with Ns
+                                 maxEE = 3, # allow w/ up to 3 expected errors
+                                 truncQ = 2, # cut off if quality gets this low
+                                 rm.phix = TRUE,
+                                 compress = TRUE,
+                                 multithread = FALSE)
+# produce nicely-formatted markdown table of read counts
+# before/after trimming
+kable(filtered_output,
+      col.names = c("Reads In",
+                    "Reads Out"))
+
+# this build error models from each of the samples
+errors_forward_reads <- learnErrors(filtered_reads_path,
+                                    multithread = FALSE)
+
+# quick check to see if error models match data
+# (black lines match black points) and are generally decresing left to right
+plotErrors(errors_forward_reads,
+           nominalQ = TRUE)
+
+# get rid of any duplicated sequences
+dereplicated_forward_reads <- derepFastq(filtered_reads_path)
+
+# Name the derep-class objects by the sample names
+names(dereplicated_forward_reads) <- sample_names
+
+# parameters adjusted based on recommendations for 454 data here:
+# https://benjjneb.github.io/dada2/
+#     faq.html#can-i-use-dada2-with-my-454-or-ion-torrent-data
+dada_forward_reads <- dada(dereplicated_forward_reads,
+                           err = errors_forward_reads,
+                           HOMOPOLYMER_GAP_PENALTY = -1, # reduce penalty bc 454
+                           BAND_SIZE = 32) # performs local alignments bc indels
+
+# check dada results
+dada_forward_reads
+invisible(capture.output(dereplicated_forward_reads <-
+                           derepFastq(filtered_reads_path)))
+
+# produce the 'site by species matrix'
+sequence_table <- makeSequenceTable(dada_forward_reads)
+
+# histogram of read lengths
+# Quick check to look at distribution of trimmed and denoised sequences
+hist(nchar(getSequences(sequence_table)),
+     main = "Histogram of fingal sequence variant lengths",
+     xlab = "Sequence length in bp")
+
+# Check for and remove chimeras
+sequence_table_nochim <- removeBimeraDenovo(sequence_table,
+                                            method = "consensus",
+                                            multithread = FALSE,
+                                            verbose = TRUE)
+
+# What percent of our reads are non-chimeric?
+non_chimeric_reads <- round(sum(sequence_table_nochim) / sum(sequence_table),
+                            digits = 4) * 100
+
+# pipeline table of chimeric read counts
+# Build a table showing how many sequences remain at each step of the pipeline
+get_n <- function(x) sum(getUniques(x)) # make a quick function
+track <- cbind(filtered_output, # already has 2 columns
+               sapply(dada_forward_reads, get_n),
+               rowSums(sequence_table),
+               rowSums(sequence_table_nochim))
+
+# add nice meaningful column names
+colnames(track) <- c("Input",
+                     "Filtered",
+                     "Denoised",
+                     "Sequence Table",
+                     "Non-chimeric")
+
+# set the proper rownames
+rownames(track) <- sample_names
+
+# produce nice markdown table of progress through the pipeline
+kable(track)
+
+
